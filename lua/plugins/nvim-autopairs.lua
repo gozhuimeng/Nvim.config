9ca2c1a7ca394ec5381913e45672402223a9077e
meng <gozhuimeng@outlook.com>
2025-08-30 19:51:28 +0800

    03.250830 添加两个与括号引号标签补全修改的插件

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,5 @@
+return {  -- 自动补全括号引号
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+}
---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,5 @@
+return {  -- 插入修改括号引号标签
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    opts = {},
+}
