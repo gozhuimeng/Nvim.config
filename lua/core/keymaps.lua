e88a86246a1d0e751f64925a416662d039e5fd96
meng <gozhuimeng@outlook.com>
2025-09-10 10:43:20 +0800

    14.250910 修正jk退出i模式的opts，添加取消搜索高亮快捷键

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,6 @@
 -- Insert mod
-vim.keymap.set("i", "jk", "<Esc>", opts)
+vim.keymap.set("i", "jk", "<Esc>", { silent = true })
+
+-- clean search highlight
+vim.keymap.set("n", "<C-l>", ":nohlsearch<CR>", { silent = true })
+vim.keymap.set("i", "<C-l>", "<cmd>nohlsearch<CR>", { silent = true })
