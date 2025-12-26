edc6923cb088c31838c94ca33ca1343a585358cb
meng <gozhuimeng@outlook.com>
2025-12-27 03:28:47 +0800

    21.251227 新增插件Nvim-tree，删除autocmd中关于切换输入法的是否启用的变量is_enable

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,13 +1,20 @@
 -- 修改系统输入法，此方案只在linux fcitx中生效
-local is_enable = true
-
 if vim.fn.executable("fcitx5-remote") == 1 then
 	vim.api.nvim_create_autocmd("InsertLeave", {
 		callback = function()
-			if is_enable then
-				vim.fn.system("fcitx5-remote -c") -- 切换至英文模式
-			end
+			vim.fn.system("fcitx5-remote -c") -- 切换至英文模式
 		end,
 		desc = "退出插入模式的同时切换至英文输入法",
 	})
 end
+
+-- 使用nvim打开目录的时候是否使用Nvim-tree替代原有的netrw
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local path = vim.fn.argv(0)
+		if path ~= "" and vim.fn.isdirectory(path) == 1 then
+			vim.cmd("bd")
+			require("nvim-tree.api").tree.open()
+		end
+	end,
+})
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,49 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+	keys = {
+		{ "<leader>tt", "<cmd>NvimTreeToggle<CR>", desc = "打开/关闭文件树" },
+		{ "<leader>tF", "<cmd>NvimTreeFocus<CR>", desc = "聚焦到Nvim-tree" },
+		{ "<leader>to", "<cmd>NvimTreeOpen<CR>", desc = "打开Nvim-tree" },
+		{ "<leader>tf", "<cmd>NvimTreeFindFile<CR>", desc = "定位到当前文件" },
+	},
+	config = function()
+		local api = require("nvim-tree.api")
+
+		local function on_attach(bufnr)
+			-- 原有配置
+			api.config.mappings.default_on_attach(bufnr)
+
+			-- 自定义配置
+			vim.keymap.set("n", "l", api.node.open.edit, { buffer = bufnr, desc = "展开目录" })
+			vim.keymap.set("n", "h", api.node.navigate.parent_close, { buffer = bufnr, desc = "收起目录" })
+			vim.keymap.set("n", "v", api.node.open.vertical, { buffer = bufnr, desc = "水平分屏打开文件" })
+			vim.keymap.set("n", "s", api.node.open.horizontal, { buffer = bufnr, desc = "垂直分屏打开文件" })
+		end
+
+		require("nvim-tree").setup({
+			actions = {
+				open_file = {
+					quit_on_open = false, -- 打开文件后自动关闭Nvim-tree
+				},
+			},
+			filters = {
+				dotfiles = false, -- 不隐藏点文件
+				custom = {}, -- 隐藏目录
+			},
+			update_focused_file = {
+				enable = true, -- 高亮显示当前文件
+			},
+			git = {
+				enable = true, --显示git状态
+			},
+			diagnostics = {
+				enable = true, -- 显示错误/警告标记
+			},
+			on_attach = on_attach, -- 配置自定义按键
+		})
+	end,
+}
