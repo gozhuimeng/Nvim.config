4f2e742f83e53f01c980f516439352b7b528c55b
meng <gozhuimeng@outlook.com>
2026-02-28 21:51:23 +0800

    feat(spellfile): 添加spell相关功能
    - 更新README.md，添加spell相关介绍
    - 添加autocmd
      - 指定spellfile路径
      - 每次启动nvim检查二进制文件是否缺失
    - 添加user_cmd
      - 可以通过`RecompileAllSpell`编译所有spell索引
    - 添加了少量lua和全局spell

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 ### 备注
 
 #### nvim-treesitter
-- 需要额外安装`tree-sitter-cli`
 
+- 需要额外安装`tree-sitter-cli`
 - 添加parser后首次启动需要等待小段时间，并且重新启动才能成功加载Treesitter
 
 #### tv.nvim(television.nvim)
-- 需要额外安装`television`或是参考文档拉去二进制文件
 
+- 需要额外安装`television`或是参考文档拉取二进制文件
+
+#### spell
+
+- 每次进入Nvim都将会自动编译缺失`.spl`二进制文件
+- 二进制`.spl`如果和`.add`文件版本不一致或功能异常，请使用`:RecompileAllSpell`重新编译所有文件或者通过`:mkspell! <filepath>`编译指定的spell
--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -18,3 +18,45 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		end
 	end,
 })
+
+-- spell
+-- spellfile's path
+local base_dir = vim.fn.stdpath("config") .. "/lua/core/spell/"
+
+-- Auto create spellfile for different filetype
+local spell_group = vim.api.nvim_create_augroup("AutoSpell", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	group = spell_group,
+	pattern = "*",
+	callback = function(args)
+		local ft = args.match
+
+		local ft_spell = base_dir .. ft .. ".utf-8.add"
+		local global_spell = base_dir .. "global.utf-8.add"
+
+		vim.fn.mkdir(base_dir, "p")
+
+		vim.opt_local.spell = true
+		vim.opt_local.spellfile = { ft_spell, global_spell }
+	end,
+})
+
+-- Check if the binary file exists, and compile the nonexistent file
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		for _, file in ipairs(vim.fn.globpath(base_dir, "*.add", true, true)) do
+			local spl = file .. ".spl"
+			if vim.fn.filereadable(file) == 1 and vim.fn.filereadable(spl) == 0 then
+				vim.cmd("silent mkspell!" .. file)
+			end
+		end
+	end,
+})
+
+-- create cmd for compiling all spell files.
+vim.api.nvim_create_user_command("RecompileAllSpell", function()
+	for _, f in ipairs(vim.fn.globpath(base_dir, "*.add", true, true)) do
+		vim.cmd("mkspell!" .. f)
+	end
+	print("All spell compiled!")
+end, { desc = "Recompile all spell files" })
