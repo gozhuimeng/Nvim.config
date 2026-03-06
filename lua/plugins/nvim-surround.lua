7bc0f433f2b5fea6f314202b76e3f3b04a4b7f87
meng <gozhuimeng@outlook.com>
2026-03-06 10:27:36 +0800

    fix(nvim-surround): 添加插件nvim-surround的按键说明

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,3 +3,15 @@ return { -- 插入修改括号引号标签
 	event = "VeryLazy",
 	opts = {},
 }
+
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     surr*ound_words             ysiw(           ( surround_words )
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
